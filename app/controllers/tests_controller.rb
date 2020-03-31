3c8bcabe12ff38e682ed7af0f12689c90e1d428e
darkelv <darkelv777@yandex.ru>
2020-03-31 21:27:35 +0300

    lesson_20

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,10 +2,16 @@ class TestsController < Simpler::Controller
 
   def index
     @time = Time.now
+    @tests = Test.all
+    # render 'tests/list'
   end
 
   def create
 
+  end
+
+  def show
+    @test = Test.find(id: params[:id])
   end
 
 end
